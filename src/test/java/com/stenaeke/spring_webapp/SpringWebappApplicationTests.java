370f3608b94fcab8b4c68bd7890fc4752acb59d8
Casper <casper.andersson@capgemini.com>
2025-10-24 14:36:28 +0200

    initial commit

---
+++ src/test/java/com/stenaeke/spring_webapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stenaeke.spring_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
